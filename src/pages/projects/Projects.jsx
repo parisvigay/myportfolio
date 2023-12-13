fa6183e08a7da8632f2f7430453323fdce89c0bd
Paris Vigay <parisvigay@Pariss-MacBook-Pro.local>
2023-12-13 12:51:42 +0000

    13/12

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -12,13 +12,13 @@ export default function Projects() {
           readme="https://github.com/parisvigay/mastermindGame/blob/main/README.md"
         />
         <ProjectCard 
-          title="TuneIn app"
+          title="TuneIn"
           bgimg="https://i.ibb.co/xhD3rrz/Screenshot-2023-11-29-at-12-53-51.png"
           link="https://tunein-app.netlify.app/"
           readme="https://github.com/parisvigay/unit_2_project_frontend/blob/main/README.md" 
         />
         <ProjectCard 
-          title="Jammin app (deployment I.P)"
+          title="Jammin (deploying)"
           bgimg="https://i.ibb.co/gWzDBDN/Screenshot-2023-12-07-at-14-02-23.png"
           link="https://parisvigay.netlify.app/projects"
           readme="https://parisvigay.netlify.app/projects"
