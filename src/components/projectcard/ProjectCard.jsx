0155ce781888364db94399f7dce65871d946ae40
Paris Vigay <parisvigay@Pariss-MacBook-Pro.local>
2024-01-03 16:55:59 +0000

    03/01/24

--- src/components/projectcard/ProjectCard.jsx
+++ src/components/projectcard/ProjectCard.jsx
@@ -1,17 +1,37 @@
 import './ProjectCard.css'
+import { useState } from 'react';
 import { Card } from "@mui/joy";
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 
-export default function ProjectCard({ title, bgimg, link, readme }) {
+export default function ProjectCard({ title, bgimg, link, readme, projectInfo }) {
+  const [expandedCard, setExpandedCard] = useState(false);
+
+  const expandCard = () => {
+    setExpandedCard(prevExpanded => !prevExpanded);
+  }
+
   const backgroundStyle = {
     backgroundImage: `url(${bgimg})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center'
   };
   return (
-    <Card id="projectCard">
-        <h2 className="projectTitle">{title}</h2>
-        <a href={link} target="_blank" rel="noreferrer"><Card id="imgCard" style={backgroundStyle}></Card></a>
-        <a href={readme} target="_blank" rel="noreferrer" className="readMe">README</a>
-    </Card>
+    <div className="cardsContainer">
+      <Card variant="plain" id={expandedCard ? "expandedProjectCard" : "projectCard"}>
+        <a href={link} style={{ textDecoration: "none" }} target="_blank" rel="noreferrer"><h2 className="projectTitle">{title}</h2></a>
+          <Card id="imgCard" style={backgroundStyle}></Card>
+          <a href={readme} target="_blank" rel="noreferrer" className="readMe">README</a>
+          {!expandedCard && (
+          <KeyboardDoubleArrowDownIcon id="projectDropDown" onClick={expandCard}/>
+          )}
+      </Card>
+      {expandedCard && (
+            <Card variant="plain" id="extraCard">
+              {projectInfo}
+              <KeyboardDoubleArrowUpIcon id="projectDropDownUp" onClick={expandCard}/> 
+            </Card>
+          )}
+    </div>
   )
 }
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -10,24 +10,28 @@ export default function Projects() {
           bgimg="https://i.ibb.co/QrdWJFx/Screenshot-2023-11-29-at-12-33-26.png"
           link="https://parisvigay.github.io/mastermindGame/"
           readme="https://github.com/parisvigay/mastermindGame/blob/main/README.md"
+          projectInfo="My first project, a simple browser game made with HTML, CSS and vanilla Javascript. It combines logic with aesthetic to produce a clean, user-friendly version of the popular code breaking game, Master Mind."
         />
         <ProjectCard 
           title="TuneIn"
           bgimg="https://i.ibb.co/xhD3rrz/Screenshot-2023-11-29-at-12-53-51.png"
           link="https://tunein-app.netlify.app/"
-          readme="https://github.com/parisvigay/unit_2_project_frontend/blob/main/README.md" 
+          readme="https://github.com/parisvigay/unit_2_project_frontend/blob/main/README.md"
+          projectInfo="My second project, a music-based social media app, built using Vue.js for the frontend, and Express js for the backend. It allows users to recommend artists, songs and albums to other users and to express their musical favourites" 
         />
         <ProjectCard 
           title="Jammin (deploying)"
           bgimg="https://i.ibb.co/gWzDBDN/Screenshot-2023-12-07-at-14-02-23.png"
           link="https://parisvigay.netlify.app/projects"
           readme="https://parisvigay.netlify.app/projects"
+          projectInfo="My third project was an application built for bands to track their songs and rehearsals. It was built with a React.js frontend and a Python Django backend."
         />
         <ProjectCard 
-          title="P Alexander (W.I.P)"
+          title="Lotties Coffee (W.I.P)"
           bgimg=""
           link="https://parisvigay.netlify.app/projects"
           readme="https://parisvigay.netlify.app/projects"
+          projectInfo="A freelance project, coding the website for a local cafe. (W.I.P)"
         />
       </div>
     </div>
